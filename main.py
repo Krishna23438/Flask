db429fafb1d0fad84c261516a1f54ff247937613
Delta Student <krishnagupta2402@gmail.com>
2026-07-31 23:58:59 +0530

    commit

--- main.py
+++ main.py
@@ -11,30 +11,33 @@ app = Flask(__name__)
 #   #print(url_for("static",filename="static.css"))
 #   return render_template("index.html",name = username,sub=subject)
 
+# @app.route("/")
+# def hello_world():
+#    data = {
+#       "message":"Welcome to the platform"
+#    }
+
+#    return jsonify(data),501
+
 @app.route("/")
 def hello_world():
-   data = {
-      "message":"Welcome to the platform"
-   }
+  return render_template("index.hmtl")
+# @app.route("/login",methods = ["GET","POST"])
+# def login():
+#   if request.method == "POST":
+#       #print(request.form) #ImmutableMultiDict([('username', 'Krishna Gupta'), ('password', '123')])
+#       name = request.form["username"]
+#       password = request.form["password"]
 
-   return jsonify(data),501
+#       freinds = ["Bob", "Adam", "Charlie","Sam"]
 
-@app.route("/login",methods = ["GET","POST"])
-def login():
-  if request.method == "POST":
-      #print(request.form) #ImmutableMultiDict([('username', 'Krishna Gupta'), ('password', '123')])
-      name = request.form["username"]
-      password = request.form["password"]
-
-      freinds = ["Bob", "Adam", "Charlie","Sam"]
-
-      header = "<header>ABC Website</header>"
-      return render_template("welcome.html",name = name, password = password, freinds = freinds, header = header)
+#       header = "<header>ABC Website</header>"
+#       return render_template("welcome.html",name = name, password = password, freinds = freinds, header = header)
       
-  else:
-    return render_template("login.html")
+#   else:
+#     return render_template("login.html")
 
-  #return "<p> this route is to handle login</p>"
+#   #return "<p> this route is to handle login</p>"
 
 
 if __name__ == "__main__":
